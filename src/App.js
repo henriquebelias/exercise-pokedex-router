142b9644ee92c0f369551a1db602a2955c0685a8
Henrique Brito Elias <henriquebelias@gmail.com>
2021-03-04 17:40:52 -0300

    Exercicios 4 e 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Pokedex from './Pokedex';
 import PokemonDetails from './components/PokemonDetails';
+import About from './components/About';
 import './App.css';
 import pokemons from './data';
 
@@ -10,12 +11,17 @@ function App() {
     <div className="App">
       <h1> Pokedex </h1>
       <BrowserRouter>
-      <Link to="/">Home</Link>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
         <Switch>
           <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
           <Route
             path="/pokemon/:id/details"
-            render={(props) => <PokemonDetails {...props} pokemons={pokemons} />} />
+            render={(props) => <PokemonDetails {...props} pokemons={pokemons} />}
+          />
+          <Route path="/about" component={About} />
         </Switch>
       </BrowserRouter>
     </div>
